cf3ddbfe524bde26f606d1dc30103bc205285637
Arman Ebtekari <93611871+Armanebtekari@users.noreply.github.com>
2022-02-13 23:41:26 +0330

    Update index.py

--- index.py
+++ index.py
@@ -10,12 +10,9 @@ click_type = driver.find_element_by_xpath('//*[@id="login_field"]')
 click_type.click()
 click_type.send_keys(input('whats yore Gmail? '))
 
-
-
 click_type_password = driver.find_element_by_xpath('//*[@id="password"]')
 click_type_password.click()
 click_type_password.send_keys(input('whats yore password? '))
-
 
 submit_list = driver.find_element_by_xpath('//*[@id="login"]/div[4]/form/div/input[12]')
 submit_list.click()
@@ -24,7 +21,7 @@ driver.get('https://github.com/Armanebtekari/comit/edit/main/README.md')
 
 commit = driver.find_element_by_xpath('//*[@id="new_blob"]/div/file-attachment/div/div[2]/div/textarea')
 commit.click()
-commit.send_keys('arman')
+commit.send_keys('commit')
 
 display = driver.find_element_by_xpath('//*[@id="new_blob"]/div/div[1]/span/input')
 display.click()
@@ -37,7 +34,7 @@ driver.get('https://github.com/Armanebtekari/comit/edit/main/README.md')
 
 commit = driver.find_element_by_xpath('//*[@id="new_blob"]/div/file-attachment/div/div[2]/div/textarea')
 commit.click()
-commit.send_keys('arman')
+commit.send_keys('commit')
 
 display = driver.find_element_by_xpath('//*[@id="new_blob"]/div/div[1]/span/input')
 display.click()
@@ -51,7 +48,7 @@ for x in range(10):
 
     commit = driver.find_element_by_xpath('//*[@id="new_blob"]/div/file-attachment/div/div[2]/div/textarea')
     commit.click()
-    commit.send_keys('arman')
+    commit.send_keys('commit')
 
     display = driver.find_element_by_xpath('//*[@id="new_blob"]/div/div[1]/span/input')
     display.click()
